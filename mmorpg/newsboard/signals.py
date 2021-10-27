ac824051d7b8a07f82cd4ff7cd736d96e5b9f15b
pten4uk <pten4ik99@yandex.ru>
2021-10-27 19:32:14 +0300

    Begun send email messages

--- mmorpg/newsboard/signals.py
+++ mmorpg/newsboard/signals.py
@@ -1,5 +1,7 @@
+from django.core.mail import EmailMultiAlternatives
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.template.loader import render_to_string
 
 from newsboard.models import Comment
 
@@ -7,4 +9,21 @@ from newsboard.models import Comment
 @receiver(post_save, sender=Comment)
 def send_mail(sender, instance, created, **kwargs):
     if not created:
-        pass
+        user = instance.post.author
+
+        html = render_to_string(
+            'newspaper/send_messages/new_comment.html',
+            {
+                'user': user,
+                'comment': instance,
+             },
+        )
+
+        msg = EmailMultiAlternatives(
+                subject=f'Вы получили новый отклик!"',
+                from_email='pten4ik99@yandex.ru',
+                to=[user.email]
+            )
+
+        msg.attach_alternative(html, 'text/html')
+        msg.send()
